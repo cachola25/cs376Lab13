e0140f58f7ce1b6daf696a4081435a56024f6714
Benning <benning23@LINUX-EGR-06676.campus.up.edu>
2022-12-08 15:26:50 -0800

    Charlie Benning

--- main.c
+++ main.c
@@ -8,7 +8,7 @@
  * @since August 2013
  * @author Martin Cenek
  * @since September 2021
- * Author Charlie Benning Edit
+ * Author Charie Benning Flags Edit
  */
 #include "robot.h"
 #include <ctype.h>
